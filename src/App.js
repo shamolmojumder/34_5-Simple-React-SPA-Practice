9f0beaf60c5aaa5f4f058530d3082919eeadb1b3
unknown <shamolmojumder14@gmail.com>
2020-10-02 01:19:37 +0600

    console.log(setCountries);

--- src/App.js
+++ src/App.js
@@ -8,11 +8,24 @@ function App() {
   useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
       .then(res => res.json())
-      .then(data => setCountries(data))
+      .then(data => {
+
+
+        setCountries(data)
+
+        // console.log(data);
+        data.map(country => country.name)
+      })
+      .catch(error => console.log(error))
   }, [])
   return (
+
+
     <div className="App" >
       <h1>{countries.length}</h1>
+      <ul>
+        {countries.map(country => <li>{country.name}</li>)}
+      </ul>
     </div >
   )
 }
